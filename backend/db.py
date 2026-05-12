31d20090ccc77e29aa5870fb06bdb61c6feb7c49
MasRey <mametrehan28@gmail.com>
2026-05-12 21:04:52 +0700

    update

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,20 @@
+import os
 import psycopg2
 from config import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT
 
+
 def get_db_connection():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        database=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-        port=DB_PORT
-    )
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url:
+        conn = psycopg2.connect(database_url)
+    else:
+        conn = psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+            port=DB_PORT
+        )
+
     return conn
